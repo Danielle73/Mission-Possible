1df305e24e3e19897593d9a819aafc12f321db10
Danielle <daniellelee73@gmail.com>
2025-01-07 17:31:14 +0000

    Modified files to get rid of the blueprint code.

---
+++ mission-possible/src/components/Task.jsx
@@ -0,0 +1,7 @@
+const Task = () => {
+    return ( 
+        <h1>Hello!</h1>
+     );
+}
+ 
+export default Task;
---
+++ mission-possible/src/taskData.js
@@ -0,0 +1,34 @@
+
+const tasks = [
+    {
+      id: 1,
+      name: "Learn React",
+      completed: false,
+      description: "Study React basics and components",
+      priority: "high",
+    },
+    {
+      id: 2,
+      name: "Practice JavaScript",
+      completed: false,
+      description: "Practice core JavaScript concepts",
+      priority: "medium",
+    },
+    {
+      id: 3,
+      name: "Build a To-Do app",
+      completed: false,
+      description: "Build a to-do app using React",
+      priority: "low",
+    },
+    {
+      id: 4,
+      name: "Read a book",
+      completed: false,
+      description: "Read a software development book",
+      priority: "medium",
+    },
+  ];
+  
+  export default tasks;
+  
