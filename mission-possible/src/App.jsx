8c34ce0d747ef735edb40a2cfa9fd044f14d524a
Danielle <daniellelee73@gmail.com>
2025-01-07 17:40:23 +0000

    Seeded and rendered dummy data

--- mission-possible/src/App.jsx
+++ mission-possible/src/App.jsx
@@ -2,11 +2,24 @@ import './App.css'
 import Task from './components/Task'
 import tasks from './taskData'
 
+function createTask (taskEntry){
+
+  return(
+    <Task 
+    key = {taskEntry.id}
+    name = {taskEntry.name}
+    completed = {taskEntry.completed}
+    description = {taskEntry.description}
+    priority = {taskEntry.priority}
+    />
+  )
+}
+
 function App() {
 
   return (
     <>
-      <Task />
+    {tasks.map(createTask)}
     </>
   )
 }
--- mission-possible/src/components/Task.jsx
+++ mission-possible/src/components/Task.jsx
@@ -1,6 +1,12 @@
-const Task = () => {
+/* eslint-disable react/prop-types */
+function Task(props){
     return ( 
-        <h1>Hello!</h1>
+        <>
+        <h1>{props.name}</h1>
+        <p>{props.completed}</p>
+        <p>{props.description}</p>
+        <p>{props.priority}</p>
+        </>
      );
 }
  
