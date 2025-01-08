162c004ecdb0c967915cbbc5ce3803c660a3af9a
Danielle <daniellelee73@gmail.com>
2025-01-08 02:06:35 +0000

    Style changes made

--- mission-possible/src/components/Header.jsx
+++ mission-possible/src/components/Header.jsx
@@ -3,6 +3,7 @@ function Header(){
         <>
         <div className="header">
         <h1>Mission Possible 📝 </h1>
+        <p className="header-description"> procrastinate no more! </p>
         </div>
         </>
     )
