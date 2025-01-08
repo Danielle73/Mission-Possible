4440b7ce06143da8206ca969ef92ba412e0f4630
Danielle <daniellelee73@gmail.com>
2025-01-08 18:36:18 +0000

    Refactor of task entry function in app.jsx

--- mission-possible/src/App.jsx
+++ mission-possible/src/App.jsx
@@ -4,25 +4,25 @@ import tasks from './taskData'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-function createTask (taskEntry){
 
-  return(
-    <Task 
-    key = {taskEntry.id}
-    name = {taskEntry.name}
-    completed = {taskEntry.completed}
-    description = {taskEntry.description}
-    priority = {taskEntry.priority}
-    />
-  )
-}
 
 function App() {
 
   return (
     <>
     <Header />
-    {tasks.map(createTask)}
+    {tasks.map( (taskEntry) => {
+
+return(
+  <Task 
+  key = {taskEntry.id}
+  name = {taskEntry.name}
+  completed = {taskEntry.completed}
+  description = {taskEntry.description}
+  priority = {taskEntry.priority}
+  />
+)
+})}
     <Footer />
     </>
   )
