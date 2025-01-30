b5e3c41b411cbe1546d86cbcf57e738babe8dbe2
Danielle73 <daniellelee73@gmail.com>
2025-01-30 21:45:32 +0000

    Establishing add task function to button component

--- mission-possible/src/components/Button.jsx
+++ mission-possible/src/components/Button.jsx
@@ -1,8 +1,22 @@
+import {useState} from React;
+
 function Button(){
+
+const [task, setTask] = useState("");
+const [items, setItems] = useState([]);
+
+function handleChange(event) {
+    const newValue = event.target.value;
+
+    setTask(newValue);
+  }
+
     return (
         <>
+        <input onChange={handleChange} type="text" value={task} />
+
         <div className="button-container">
-        <button className="new-task">Add Task</button>
+        <button onClick={addItem} className="new-task">Add Task</button>
         </div>
         </>
     )
